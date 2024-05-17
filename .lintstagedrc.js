31b496960be7697a81b866f905f2463fe90ba48c
墨殇 <1651337573@qq.com>
2024-05-17 18:48:20 +0800

    test

---
+++ .lintstagedrc.js
@@ -0,0 +1,18 @@
+const { ESLint } = require('eslint');
+
+const removeIgnoredFiles = async files => {
+	const eslint = new ESLint();
+	const ignoredFiles = await Promise.all(
+		files.map(file => eslint.isPathIgnored(file)),
+	);
+	const filteredFiles = files.filter((_, index) => !ignoredFiles[index]);
+	return filteredFiles.join(' ');
+};
+
+module.exports = {
+	'*': async files => {
+		const filesToLint = await removeIgnoredFiles(files);
+		console.log('filesToLint:', filesToLint);
+		return [`eslint ${filesToLint} --max-warnings 0`];
+	},
+};
